52b9bc964bdae35c657815355e7fcc79dd712c66
squashmeister99 <squashmeister99@gmail.com>
2017-01-31 10:58:52 -0700

    initial commit of beatbox

---
+++ app/src/main/java/com/example/rajesh/beatbox/BeatBoxActivity.java
@@ -0,0 +1,11 @@
+package com.example.rajesh.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
