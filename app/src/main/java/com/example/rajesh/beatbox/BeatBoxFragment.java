3d95ac52ff2ef7ce3c9402b390accab430d8e589
squashmeister99 <squashmeister99@gmail.com>
2017-02-02 18:32:57 -0700

    beatbox updates

--- app/src/main/java/com/example/rajesh/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/rajesh/beatbox/BeatBoxFragment.java
@@ -27,6 +27,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(getActivity());
     }
@@ -41,7 +42,7 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Button mButton;
         private Sound mSound;
 
@@ -49,12 +50,18 @@ public class BeatBoxFragment extends Fragment {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
 
             mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
 
         }
 
         public void bindSound(Sound sound) {
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
 
@@ -82,5 +89,11 @@ public class BeatBoxFragment extends Fragment {
         public int getItemCount() {
             return mSounds.size();
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
     }
 }
--- app/src/main/java/com/example/rajesh/beatbox/Sound.java
+++ app/src/main/java/com/example/rajesh/beatbox/Sound.java
@@ -15,8 +15,17 @@ public class Sound {
     }
 
     private String mAssetPath;
-
     private String mName;
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
